7ed1dd125a137f7572e5a93d31cee6e623fb45fd
adarsh-m <adarshmunnanuru@gmail.com>
2021-11-11 19:26:22 -0600

    code added for Implement create

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -15,9 +15,25 @@ exports.mobile_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: mobile detail: ' + req.params.id);
 };
 // Handle mobile create on POST.
-exports.mobile_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: mobile create POST');
-};
+exports.mobile_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new mobile();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.name = req.body.name; 
+    document.type = req.body.type;
+    document.cost = req.body.cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // Handle mobile delete form on DELETE.
 exports.mobile_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: mobile delete DELETE ' + req.params.id);
@@ -25,4 +41,18 @@ res.send('NOT IMPLEMENTED: mobile delete DELETE ' + req.params.id);
 // Handle mobile update form on PUT.
 exports.mobile_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: mobile update PUT' + req.params.id);
+};
+
+
+// VIEWS
+// Handle a show all view 
+exports.mobile_view_all_Page = async function(req, res) {
+try{
+themobile = await mobile.find();
+res.render('mobile', { title: 'Mobile Search Results', results: themobile });
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
 };
--- routes/mobile.js
+++ routes/mobile.js
@@ -1,9 +1,16 @@
+/*
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('mobile', { title: 'Search Results mobile' });
 });
 
+module.exports = router;
+
+*/
+var express = require('express'); const mobile_controlers= require('../controllers/mobile');
+var router = express.Router();
+/* GET mobile */ router.get('/', mobile_controlers.mobile_view_all_Page );
 module.exports = router;
