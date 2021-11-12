5cf91356c956a427f52e28c37344621c11ca5cb3
adarsh-m <adarshmunnanuru@gmail.com>
2021-11-12 13:03:07 -0600

    code changes for lab 12

--- routes/resource.js
+++ routes/resource.js
@@ -17,4 +17,39 @@ router.put('/resource/mobile/:id', mobile_controller.mobile_update_put);
 router.get('/resource/mobile/:id', mobile_controller.mobile_detail);
 // GET request for list of all mobile items.
 router.get('/resource/mobile', mobile_controller.mobile_list);
+// Handle mobile update form on PUT.
+exports.mobile_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await mobile.findById( req.params.id)
+// Do updates of properties
+if(req.body.mobile_type)
+toUpdate.mobile_type = req.body.mobile_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.size) toUpdate.size = req.body.size;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
+// GET request for one mobile.
+router.get('/mobile/:id', mobile_controller.mobile_detail);
+// for a specific mobile.
+exports.mobile_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await mobile.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    if(req.body.checkboxsale) toUpdate.sale = true;
+else toUpdate.same = false;
+    };
 module.exports = router;
